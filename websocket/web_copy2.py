c2927d8584d54e60d994688342466b29d8045910
Gnu-kenny <Gnu-kenny@github.com>
2021-11-15 16:09:03 +0900

    fist commit

---
+++ websocket/web_copy2.py
@@ -0,0 +1,32 @@
+import asyncio
+import websockets
+import datetime
+import re
+import time
+import os
+
+
+async def time(websocket, path):
+    while True: 
+        f = open('/home/geunwoo/yolov53/yolov531/yolov5/Temp.txt', 'rt')
+        Temp = f.read()
+        f.close()
+        f = open('/home/geunwoo/yolov53/yolov531/yolov5/Mask.txt', 'rt')
+        Mask = f.read()
+        f.close()
+        
+        if len(Mask) == 0 :
+            Mask = "Not-Detected"
+        if len(Temp) == 0 :
+            Temp = "36.5"
+            
+        Data = Mask + "," + Temp
+
+        print(Data)
+        await websocket.send(Data)
+        await asyncio.sleep(0.005)
+
+start_server = websockets.serve(time, "192.168.0.69", 5501)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
